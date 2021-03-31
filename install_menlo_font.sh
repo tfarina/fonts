221d4264b4835002fd7af4b1fe17f243668cc1e8
Thiago Farina <tfransosi@gmail.com>
2021-03-31 10:24:25 -0400

    first commit
    Hack font files were downloaded and extracted from https://github.com/chrissimpkins/Hack/releases/download/v2.020/Hack-v2_020-ttf.zip.
    
    Menlo font files were downloaded and extracted from https://www.onlinewebfonts.com/package/Menlo.
    
    This is repository is to store and back up font files because I don't see them
    belonging to ~/bin repository.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ install_menlo_font.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+font_list=("Menlo-Bold.ttf" \
+           "Menlo-BoldItalic.ttf" \
+	   "Menlo-Italic.ttf" \
+           "Menlo-Regular.ttf")
+
+for file in ${font_list[*]}; do
+  echo $file  
+done
+
+font_dir=/usr/share/fonts/truetype/menlo
+
+if [ -d $font_dir ]; then
+  echo "Found directory: " $font_dir
+else
+  echo "Creating directory: " $font_dir
+  sudo mkdir -p $font_dir
+fi
+
+cd truetype/menlo
+
+echo "Installing font"
+
+for file in ${font_list[*]}; do
+  sudo chown root:root "${file}"
+  sudo cp "${file}" /usr/share/fonts/truetype/menlo
+done
+
+# Clear and regenerate your font cache and indexes.
+
+echo "Updating font cache... Please, wait."
+fc-cache -f -v
+echo "Finished. You font cache has been updated."
+
+# You can confirm that the fonts are installed with the following command:
+# fc-list | grep Menlo
