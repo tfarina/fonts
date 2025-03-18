49eab8e704dd1ff39021275ec52a9a4bba638f28
Thiago Farina <tfransosi@gmail.com>
2025-03-18 15:03:04 +0000

    add DejaVu fonts
    They are a font family based on Vera Fonts.
    
    I'm making a backup of these fonts, because I plan to use them in the future.
    
    According to Microsoft's font faq [0], I may not redistribute the Windows
    fonts and may not copy them to other computers, so I better not use them.
    
    "Redistribution and extended rights
    Apart from the document embedding rights described previously, you may not redistribute the Windows fonts. You may not copy them to other computers or servers, and you may not convert them to other formats, including bitmap formats, or modify them."
    
    This is one good reason to use DejaVu fonts instead. The other is DevaJu fonts
    have public domain license. Another reason is that they are the default fonts
    on Ubuntu 9.04 and on my system as well:
    
    > fc-match Sans
    DejaVuSans.ttf: "DejaVu Sans" "Book"
    
    > fc-match Serif
    DejaVuSerif.ttf: "DejaVu Serif" "Book"
    
    > fc-match Monospace
    DejaVuSansMono.ttf: "DejaVu Sans Mono" "Book"
    
    Bitstream Vera Fonts Copyright on the other hand, allows me to use, copy,
    publish and distribute free of charge.
    
    "Permission is hereby granted, free of charge, to any person obtaining a copy
    of the fonts accompanying this license ("Fonts") and associated documentation
    files (the "Font Software"), to reproduce and distribute the Font Software,
    including without limitation the rights to use, copy, merge, publish,
    distribute..." [1]
    
    Refs:
    [0] - https://learn.microsoft.com/en-us/typography/fonts/font-faq#redistribution-and-extended-rights
    [1] - https://dejavu-fonts.github.io/License.html
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ dejavu/install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+font_list=("DejaVuSans-BoldOblique.ttf" \
+           "DejaVuSans-Bold.ttf" \
+           "DejaVuSansCondensed-BoldOblique.ttf" \
+           "DejaVuSansCondensed-Bold.ttf" \
+           "DejaVuSansCondensed-Oblique.ttf" \
+           "DejaVuSansCondensed.ttf" \
+           "DejaVuSans-ExtraLight.ttf" \
+           "DejaVuSansMono-BoldOblique.ttf" \
+           "DejaVuSansMono-Bold.ttf" \
+           "DejaVuSansMono-Oblique.ttf" \
+           "DejaVuSansMono.ttf" \
+           "DejaVuSans-Oblique.ttf" \
+           "DejaVuSans.ttf" \
+           "DejaVuSerif-BoldItalic.ttf" \
+           "DejaVuSerif-Bold.ttf" \
+           "DejaVuSerifCondensed-BoldItalic.ttf" \
+           "DejaVuSerifCondensed-Bold.ttf" \
+           "DejaVuSerifCondensed-Italic.ttf" \
+           "DejaVuSerifCondensed.ttf" \
+           "DejaVuSerif-Italic.ttf" \
+           "DejaVuSerif.ttf")
+
+for file in ${font_list[*]}; do
+  echo $file
+done
+
+font_dir=/usr/share/fonts/truetype/dejavu
+
+if [ -d $font_dir ]; then
+  echo "Found directory: " $font_dir
+else
+  echo "Creating directory: " $font_dir
+  sudo mkdir -p $font_dir
+fi
+
+echo "Installing font..."
+
+for file in ${font_list[*]}; do
+  # TODO: do not copy the file if it already exists.
+  sudo cp "${file}" $font_dir
+done
+
+# Clear and regenerate your font cache and indexes.
+
+echo "Updating font cache... Please, wait."
+fc-cache -f -v
+echo "Finished. Your font cache has been updated."
+
+# Confirm the fonts were installed with the following command:
+# fc-list | grep DejaVu
